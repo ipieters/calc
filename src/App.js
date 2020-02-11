1a292895a632cd8a32d9a7b44a81bc286415b40d
ipieters <ipiet870@mtroyal.ca>
2020-02-11 15:42:18 -0700

    button order

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
       if (!isNaN(mathVar))
         this.setState({ input: mathVar });
       else
-        throw "ERROR".toString();  
+        throw "ERROR".toString();
     }
     catch (err) {
       this.setState({ input: "ERROR" });
@@ -55,9 +55,9 @@ class App extends Component {
             <Button handleClick={this.addToInput}>)</Button>
           </div>
           <div className="row">
-            <Button handleClick={this.addToInput}>1</Button>
-            <Button handleClick={this.addToInput}>2</Button>
-            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
             <Button handleClick={this.addToInput}>+</Button>
           </div>
           <div className="row">
@@ -67,9 +67,10 @@ class App extends Component {
             <Button handleClick={this.addToInput}>-</Button>
           </div>
           <div className="row">
-            <Button handleClick={this.addToInput}>7</Button>
-            <Button handleClick={this.addToInput}>8</Button>
-            <Button handleClick={this.addToInput}>9</Button>
+
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
             <Button handleClick={this.addToInput}>x</Button>
           </div>
           <div className="row">
